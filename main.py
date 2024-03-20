8265abb6d6c88e3ebd516aade9b80c4a93c898bf
sirbradflies <fbaldisserri@gmail.com>
2024-03-20 16:57:19 -0500

    Added get_market_info function

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from polymarket import Polymarket
 def main():
     # Fetch data
     pmarket = Polymarket()
-    markets = pmarket.get_markets(max_records=10000)
+    markets = pmarket.get_markets(max_records=100)
     enabled_markets = markets.query("enable_order_book == True")
+
     for _, market in enabled_markets.iterrows():
-        arbitrage_value = pmarket.check_arbitrage(market["tokens"])
-        print(f"{market["question"]} {arbitrage_value}")
+        token_infos = pmarket.get_market_info(market)
+        print(token_infos)
 
 
 if __name__ == "__main__":
--- polymarket.py
+++ polymarket.py
@@ -12,8 +12,31 @@ HEADER = {"Accept-Encoding": "gzip",
 
 
 class Polymarket():
-    def __init__(self):
-        self.session = requests_cache.CachedSession(expire_after=360)
+    def __init__(self, session_verify=True, session_expire_after=None):
+        self.session_verify = session_verify
+        self.session_expire_after = session_expire_after
+        self.session = requests_cache.CachedSession(expire_after=self.session_expire_after)
+
+    def get_market_info(self, market):
+        token_infos = []
+        for token in market["tokens"]:
+            token_id = token["token_id"]
+            book = self.get_book(token_id)
+            bid = float(book["bids"][-1]["price"]) if len(book["bids"]) > 0 else np.nan
+            ask = float(book["asks"][-1]["price"]) if len(book["asks"]) > 0 else np.nan
+            market_info = {"question": market["question"],
+                           "outcome": token["outcome"],
+                           "condition_id": market["condition_id"],
+                           "token_id": token_id,
+                           "end_date_iso": market["end_date_iso"],
+                           "bid": bid,
+                           "ask": ask}
+            token_infos += [market_info]
+        token_infos = pd.DataFrame.from_records(token_infos)
+        token_infos["end_date_iso"] = pd.to_datetime(token_infos["end_date_iso"]).dt.tz_localize(None)
+        token_infos.eval("spread = ask - bid", inplace=True)
+        token_infos["days_to_end"] = (token_infos["end_date_iso"] - pd.Timestamp.now()).dt.days
+        return token_infos
 
     def get_markets(self, condition_id=None, *args, **kwargs):
         return self.get_table(f"markets/{condition_id}", *args, **kwargs) if condition_id \
@@ -30,7 +53,8 @@ class Polymarket():
         return self.get_table("trades", *args, **kwargs)
 
     def get_book(self, token_id, *args, **kwargs):
-        return self.get_table("book", params={"token_id": token_id}, *args, **kwargs)
+        book = self.send_query(QUERY_URL + "book", params={"token_id": token_id})
+        return pd.Series(book)
 
     def get_table(self, table_name, params=None, max_retries=3, max_records=np.inf):
         next_cursor = None
@@ -48,12 +72,11 @@ class Polymarket():
                 print(f"{e}\nRetrying ({retries}/{max_retries})")
         return data
 
-    #TODO: Add cache to avoid re-fetching the same data
     def send_query(self, url, next_cursor=None, params=None):
         if not params: params = {}
         if next_cursor: params["next_cursor"] = next_cursor
         print(f"Sending request to {url} with params {params}")
-        response = self.session.get(url, headers=HEADER, params=params)
+        response = self.session.get(url, headers=HEADER, params=params, verify=self.session_verify)
         if response.status_code == 200:
             return response.json()
         else:
